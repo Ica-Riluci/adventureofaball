a93fe3e86050d34bf4608b1f0ce0dacae194a6e6
Ica Riluci <weiyh15@mails.tsinghua.edu.cn>
2017-07-28 10:49:05 +0800

    correct update

--- javascript/init.js
+++ javascript/init.js
@@ -188,7 +188,7 @@ function checklock() {
 
 sys.updatecareer();
 if (sys.hero(0).dis > 0)
-    $('#startgame span').innerHTML = 'Continue';
+    $('#startgame span').innerHTML = 'CONTINUE';
 /*
 function msgtest(x) {
     for (var i = 0; i < x; ++i)
